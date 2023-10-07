8de9b4665a0dc8cdb70ede9cfa5a824c2bdbdfb2
heckooo <phecko02@gmail.com>
2023-10-07 15:17:05 +0200

    working on flashcards CRUD api

---
+++ server/dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv-safe/config");
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const Deck_1 = __importDefault(require("./models/Deck"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get('/', (_, res) => {
+    res.send("hello");
+});
+app.post('/decks', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const newDeck = new Deck_1.default({
+        title: req.body.title,
+    });
+    const createdDeck = yield newDeck.save();
+    res.json(createdDeck);
+}));
+const port = parseInt(process.env.PORT) || 4000;
+mongoose_1.default.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+    app.listen(port, () => console.log(`server running on localhost:${port}`));
+});
+//# sourceMappingURL=index.js.map
---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import "dotenv-safe/config";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import Deck from "./models/Deck";
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_, res: Response) => {
+  res.send("hello");
+});
+
+app.post('/decks', async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+const port = parseInt(process.env.PORT) || 4000;
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  app.listen(port, () => console.log(`server running on localhost:${port}`));
+})
